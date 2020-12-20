9e1791c9764444dace362ef67526840f7a884bfe
Andy Chen <chenyangpoppin@hotmail.com>
2020-12-20 17:03:45 +0800

    feat: add readyState elaboration

--- src/basic.js
+++ src/basic.js
@@ -1,8 +1,17 @@
 /**
  * A basic demo of websocket
+ * 
+ * The WebSocket.readyState property returns the current state of the WebSocket connection
+ * 
+ * readyState:
+ *  0 - CONNECTING - after instantiating WebSocket
+ *  1 - OPEN       - after onopen callback is invoked
+ *  2 - CLOSING    - after ws.close() is invoked
+ *  3 - CLOSED     - after onclose callback is invoked
  */
 
 const ws = new WebSocket('wss://echo.websocket.org');
+console.log('readyState: ' + ws.readyState); // 0, CONNECTING
 
 /**
  * Build connection with wss server & send message
@@ -10,6 +19,8 @@ const ws = new WebSocket('wss://echo.websocket.org');
  * @param {*} evt 
  */
 ws.onopen = (evt) => {
+  console.log('readyState: ' + ws.readyState); // 1, OPEN
+
   console.log('Connection Open...');
   ws.send('Hello WebSockets');
 };
@@ -20,8 +31,12 @@ ws.onopen = (evt) => {
  * @param {*} evt 
  */
 ws.onmessage = (evt) => {
+  console.log('readyState: ' + ws.readyState); // 1, OPEN
+  
   console.log('Received Message: ' + evt.data);
   ws.close();
+
+  console.log('readyState: ' + ws.readyState); // 2, CLOSING
 };
 
 /**
@@ -30,6 +45,8 @@ ws.onmessage = (evt) => {
  * @param {*} evt 
  */
 ws.onclose = (evt) => {
+  console.log('readyState: ' + ws.readyState); // 3, CLOSED
+
   console.log('Connection Closed.');
 };
 
