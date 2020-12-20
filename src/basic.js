76d8e6f39e1b63a76594368c82ca15a345f56a36
Andy Chen <chenyangpoppin@hotmail.com>
2020-12-20 16:51:42 +0800

    docs: add comments to better elaborate basic example

--- src/basic.js
+++ src/basic.js
@@ -1,15 +1,34 @@
+/**
+ * A basic demo of websocket
+ */
+
 const ws = new WebSocket('wss://echo.websocket.org');
 
+/**
+ * Build connection with wss server & send message
+ * 
+ * @param {*} evt 
+ */
 ws.onopen = (evt) => {
   console.log('Connection Open...');
   ws.send('Hello WebSockets');
 };
 
+/**
+ * Receive message from wss server
+ * 
+ * @param {*} evt 
+ */
 ws.onmessage = (evt) => {
   console.log('Received Message: ' + evt.data);
   ws.close();
 };
 
+/**
+ * Invoke onclose callback when connection is closed
+ * 
+ * @param {*} evt 
+ */
 ws.onclose = (evt) => {
   console.log('Connection Closed.');
 };
